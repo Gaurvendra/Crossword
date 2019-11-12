ede8d4793db0d8245c0e19abc61d8d0dd253e3d4
Gaurvendra <44363966+Gaurvendra@users.noreply.github.com>
2019-11-12 14:10:04 +0530

    Add files via upload

---
+++ assets/js/button.js
@@ -0,0 +1,12 @@
+
+document.getElementById("redirect_signup").onclick = function () {
+    location.href = "./signup.html";
+};
+
+document.getElementById("redirect_signin").onclick = function () {
+    location.href = "./signin.html";
+};
+
+document.getElementById("redirect_leaderboard").onclick = function () {
+    location.href = "./leaderboard.php";
+};
